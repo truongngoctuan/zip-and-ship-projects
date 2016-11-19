156ddb6cda68317a9a207db98875972fac8eea3c
TRUONG Ngoc Tuan <tntuan0712494@gmail.com>
2016-11-19 11:00:59 +0700

    basic zipper module

---
+++ module-01-zipper.js
@@ -0,0 +1,51 @@
+// require modules
+var fs = require('fs');
+var archiver = require('archiver');
+
+//https://mushfiq.me/2014/08/21/node-js-script-to-make-a-zip-archive/
+exports.exec = function zipProject(dirname, zippedName) {
+
+  // create a file to stream archive data to.
+  var output = fs.createWriteStream(zippedName);
+  var archive = archiver('zip', {
+      store: true // Sets the compression method to STORE.
+  });
+
+  // listen for all archive data to be written
+  output.on('close', function() {
+    console.log(archive.pointer() + ' total bytes');
+    console.log('archiver has been finalized and the output file descriptor has closed.');
+  });
+
+  // good practice to catch this error explicitly
+  archive.on('error', function(err) {
+    throw err;
+  });
+
+  // pipe archive data to the file
+  archive.pipe(output);
+
+  // // append a file from stream
+  // var file1 = __dirname + '/file1.txt';
+  // archive.append(fs.createReadStream(file1), { name: 'file1.txt' });
+
+  // // append a file from string
+  // archive.append('string cheese!', { name: 'file2.txt' });
+
+  // // append a file from buffer
+  // var buffer3 = new Buffer('buff it!');
+  // archive.append(buffer3, { name: 'file3.txt' });
+
+  // // append a file
+  // archive.file('file1.txt', { name: 'file4.txt' });
+
+  // append files from a directory
+  archive.directory(dirname + "/");
+
+  // // append files from a glob pattern
+  // archive.glob('subdir/*.txt');
+
+  // finalize the archive (ie we are done appending files but streams have to finish yet)
+  archive.finalize();
+
+}
---
+++ zip-folder-excludes-list.js
@@ -0,0 +1,25 @@
+// require modules
+//var fs = require('fs');
+var jsonfile = require('jsonfile');
+var zipper = require('./module-01-zipper.js');
+
+//get params
+var __dirname = process.argv[2];
+process.argv.forEach(function (val, idx) {
+  console.log(val + " : " + idx);
+});
+
+//get configuration object from a config file
+var configObject = {};
+jsonfile.readFile('folder-list.json', function (error, obj) {
+  configObject = obj;
+  console.log(configObject);
+
+  zipAndShip(configObject);
+});
+
+function zipAndShip(configObject) {
+  configObject.projects.forEach(function (projectObject, idx) {
+    zipper.exec(__dirname, projectObject.filename);
+  });
+}
