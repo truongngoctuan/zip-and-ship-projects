372a8c9bf937736bcae941b26f7ed5e0ea4231ab
TRUONG Ngoc Tuan <tntuan0712494@gmail.com>
2017-04-06 18:31:06 +0700

    can zip packages into assets folder

---
+++ module-01-02-pacgakes-sharer.js
@@ -0,0 +1,80 @@
+// require modules
+const path = require('path');
+var fs = require('fs');
+var archiver = require('archiver');
+var pretty = require('prettysize');
+var glob = require('glob');
+
+//inForder: folder the contain `packages`, asked to be synced
+//outFolder: where I put zipped nuget package files
+exports.exec = function progress(inFolder, outFolder) {
+
+    var dirs = getDirectories(path.join(inFolder, '/packages'));
+    var existingDirs = getFiles(path.join(outFolder));
+
+    dirs.forEach(function(dir) {
+        if (existingDirs.indexOf(dir + '.zip') == -1) {
+            zipThatFolderForMe(path.join(inFolder, '/packages', dir), outFolder); 
+        }
+          
+    }, this);
+}
+
+function getDirectories (srcpath) {
+  return fs.readdirSync(srcpath)
+    .filter(file => fs.statSync(path.join(srcpath, file)).isDirectory())
+}
+
+function getFiles (srcpath) {
+  var filesWithPath = glob.sync(srcpath + '/**/*.*', {
+      base: srcpath
+  }); //TODO: improve
+
+  var files = [];
+  filesWithPath.forEach(function (filesWithPath) {
+      files.push(path.basename(filesWithPath));
+  });
+  return files;
+}
+
+function getFolderNameWithoutPath(folderPath) { //TODO: path.basename ?
+    var forwardflashIdx = folderPath.lastIndexOf('\\');
+    if (forwardflashIdx != -1) {
+        return folderPath.substr(forwardflashIdx + 1);
+    }
+    return folderPath;
+}
+
+function zipThatFolderForMe(folderName, outFolder) {
+    var fileExt = 'zip';
+  var zippedName = getFolderNameWithoutPath(folderName) + '.' + fileExt;
+  console.log('folderName = ' + folderName);
+  //console.log('zippedName = ' + zippedName);
+  //console.log('out file = ' + path.join(outFolder, zippedName));
+
+  // create a file to stream archive data to.
+  var output = fs.createWriteStream(path.join(outFolder, zippedName));
+  var archive = archiver(fileExt, {
+      store: true // Sets the compression method to STORE.
+  });
+
+  // listen for all archive data to be written
+  output.on('close', function() {
+    console.log("zip package (" + pretty(archive.pointer()) + ") " + zippedName);
+  });
+
+  // good practice to catch this error explicitly
+  archive.on('error', function(err) {
+    throw err;
+  });
+
+  // pipe archive data to the file
+  archive.pipe(output);
+
+  archive.glob('./**/*.*', {
+    cwd: folderName
+  });
+
+  // finalize the archive (ie we are done appending files but streams have to finish yet)
+  archive.finalize();
+}
--- zip-folder-excludes-list.js
+++ zip-folder-excludes-list.js
@@ -1,13 +1,10 @@
 // require modules
-//var fs = require('fs');
 var jsonfile = require('jsonfile');
 var zipper = require('./module-01-zipper.js');
+var package_handler = require('./module-01-02-pacgakes-sharer');
 
 //get params
 var __dirname = process.argv[2];
-// process.argv.forEach(function (val, idx) {
-//   console.log(val + " : " + idx);
-// });
 
 //get configuration object from a config file
 var configObject = {};
@@ -24,5 +21,7 @@ function zipAndShip(configObject) {
       projectObject.source, 
       projectObject.filename, 
       projectObject.includes);
+    
+    package_handler.exec(projectObject.source, 'assets');
   });
 }
